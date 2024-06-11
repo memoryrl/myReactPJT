676502e2fbeded907c6eb102e1527e910e1b36ef
kimnamcheol <memoryrl@kaoni.com>
2024-06-11 17:31:27 +0900

    리액트 4일차

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from "react";
+// import axios from "../node_modules/axios/index";
 
-function App() {
+// const App = () => {
+//   const [data, setData] = useState(null);
+//   const onClick = async () => {
+    
+//     // const response = axios.get('http://jsonplaceholder.typicode.com/todos/1').then(response => {
+//     //   setData(response.data);
+//     // });
+
+//     try {
+//       const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=48c8485bb51b4e3ab4625983b0a343d6');
+
+//       setData(response.data);
+//     } catch (e) {
+//       console.log(e);
+//     }
+    
+//   };
+
+//   return (
+//     <div>
+//       <div>
+//         <button onClick={onClick}>불러오기</button>
+//       </div>
+//       {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/>}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// import { useState, useCallback } from "react";
+// import NewsList from "./components/NewsList";
+// import Categories from "./components/Categories";
+
+// const App = () => {
+
+//   const [category, setCategory] = useState('all');
+//   const onSelect = useCallback(category => setCategory(category), []);
+
+//   return (
+//     <>
+//       <Categories category={category} onSelect={onSelect}/>
+//       <NewsList category={category}/>
+//     </>
+//   );
+// };
+
+import { Route, Routes } from "react-router-dom";
+import NewsPage from "./pages/NewsPage";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<NewsPage/>}/>
+      <Route path="/:category" element={<NewsPage/>}/>
+    </Routes>
   );
-}
+};
 
 export default App;
